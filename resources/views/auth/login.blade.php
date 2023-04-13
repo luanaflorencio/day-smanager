fb22bd624348855c03bb9e3b23efc8be4b601d46
Gabriel Henrique <gabrieltjs75@gmail.com>
2023-04-12 23:04:46 -0300

    borda input pagina login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,21 +2,20 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
+    <style>
+        #email:focus, #email::selection {
+            outline: none;
+            border: 2px solid red;
+        }
+    </style>
+
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <style>
-
-
-            #email, #email:focus{
-                border: .0625rem solid #6e13b6;
-            }
-        </style>
-
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('')"/>
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="User"/>
+            <x-input-label for="email" :value="__('')" />
+            <x-text-input style="border-color:#6e13b6; border-width: 1px;" id="email" class="block mt-1 w-full inputL" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="User" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -24,12 +23,7 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password"
-                            placeholder="Password"
-                            style="border-color:#6e13b6; border-width: 1px;"/>
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" placeholder="Password" style="border-color:#6e13b6; border-width: 1px;" />
 
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
@@ -45,9 +39,9 @@
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                {{ __('Forgot your password?') }}
+            </a>
             @endif
 
             <x-primary-button class="ml-3" style="background-color:#ff8e01;">
